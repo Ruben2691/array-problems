04e3a192c90d5a3bc5151f8f623f8415bf171535
Ruben Cuello <kuelmusic@gmail.com>
2024-05-11 15:37:11 -0400

    2

--- array-practice.js
+++ array-practice.js
@@ -6,30 +6,65 @@ const findMinimum = arr => {
   }
 };
 
+// time complexity of this is O(n)
+// the space complexity of this is also O(n) because of the return arr
 const runningSum = arr => {
-
-  // Your code here
+  let newArr = []
+  let sum = 0
+  for (const ele of arr) {
+    sum += ele
+    newArr.push(sum)
+  }
+  return newArr
 };
 
+// time complexity of this is O(n)
+// space complexity of this is O(1)
 const evenNumOfChars = arr => {
-
-  // Your code here
+  let counter = 0
+  for (const ele of arr) {
+    if(ele.length % 2=== 0){
+      counter++
+    }
+  }
+  return counter
 };
 
 const smallerThanCurr = arr => {
-
-  // Your code here
-
+  let newArr = []
+  let counter = 0
+  for (let i = 0; i < arr.length; i++) {
+    const element1 = arr[i];
+    for (let j = i + 1; j < arr.length; j++) {
+      const element2 = arr[j];
+      if(element1 > element2) counter++
+    }
+    newArr.push(counter)
+    counter = 0
+  }
+  return newArr
 };
 
+// time complexity of this is O(n^2) because it has to loop over the arr more than once
+// space complexity of this is O(1) because it is not storing anything
 const twoSum = (arr, target) => {
-
-  // Your code here
+ for (let i = 0; i < arr.length; i++) {
+  const element1 = arr[i];
+  for (let j = i + 1; j < arr.length; j++) {
+    const element2 = arr[j];
+    if(element1 + element2 === target){
+      return true
+    }
+  }
+ }
+ return false
 };
 
-const secondLargest = arr => {
-
-  // Your code here
+// time complexity of this is O(n log n) because of the sort method
+// space complexity of this is O(n) because im storing the sorted arr in a var
+const secondLargest = (arr)=> {
+  let sorted = arr.sort((a,b) => a-b)
+  return sorted[sorted.length - 2]
 };
 
 const shuffle = (arr) => {
@@ -37,5 +72,7 @@ const shuffle = (arr) => {
   // Your code here
 };
 
+// smallerThanCurr([8,1,2,2,3])
+// secondLargest([1, 2, 3, 4])
 
 module.exports = [findMinimum, runningSum, evenNumOfChars, smallerThanCurr, twoSum, secondLargest, shuffle];
